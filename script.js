6af66a53d2675e21813cb29aa8b5fbc4ab6ca916
kunalvmore <mkunal065@gmail.com>
2020-01-28 13:35:47 +0530

    add clickevent on nav-link

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.querySelector('.menu-icon').addEventListener('click', function () {
   document.querySelector('.navbar-nav').classList.toggle('show');
 });
+
+
+document.querySelector('.navlink1').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+document.querySelector('.navlink2').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+document.querySelector('.navlink3').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+document.querySelector('.navlink4').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+document.querySelector('.navlink5').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+document.querySelector('.navlink6').addEventListener('click', function () {
+  document.querySelector('.navbar-nav').classList.toggle('show');
+});
+
